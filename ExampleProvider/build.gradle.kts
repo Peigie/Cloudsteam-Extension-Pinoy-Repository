02fdfc87b125b17bd1d96da1a356095401efa3f7
Peigie <150321365+Peigie@users.noreply.github.com>
2023-12-14 20:16:03 +0800

    Add files via upload

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,24 +1,24 @@
 // use an integer for version numbers
-version = 1
+version = 3
 
 
 cloudstream {
+    language = "tl"
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
+    description = "Includes PinoyMoviePediaProvider and PinoyMoviesEsProvider"
+    authors = listOf("Jace")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
     status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AsianDrama"
+    )
 
-    // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of avaliable types here:
-    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Others")
-}
+    }
